ce789b1d7c7596760602c5e32798e4510c70a4ae
rajat mehta <rajatmehta.work@gmail.com>
2023-10-28 18:43:04 +0530

    added doubly LL

--- lru/sequence.go
+++ lru/sequence.go
@@ -1,46 +1,47 @@
 package lru
 
-type Sequence[V any] interface {
-	Append(value V) *ddNode
-	Evict(node *ddNode)
-	GetValue(node *ddNode)
+type sequence[V any] interface {
+	append(V)
+	delete(ddlNode[V])
+	getValue(ddlNode[V]) (V, error)
 }
 
-type dll struct {
-	dLLHead *ddNode[V]
-	dLLTail *ddNode[V]
+type dll[V any] struct {
+	dLLHead ddlNode[V]
+	dLLTail ddlNode[V]
 }
 
+func newSeq[V any]() sequence[V] {
+	return dll[V]{}
+}
 
-func(s *dll)Append[V any](value V)*ddNode{
-	newNode:=&ddNode[V]{value:value}
-	if dLLHead==nil{
-		dLLHead=newNode
-		dLLTail=newNode
-	}else{
-		newNode.prev=dLLTail
-		dLLTail=newNode
+func (s dll[V]) append(v V) {
+	newNode := &node[V]{value: v}
+	if s.dLLHead == nil {
+		s.dLLHead = newNode
+		s.dLLTail = newNode
+	} else {
+		newNode.prev = s.dLLTail
+		s.dLLTail = newNode
 	}
 }
 
-func(s *dll)Evict(node *ddNode){
-	if dLLHead==node{
-		dLLHead=nil
-		dLLTail=nil
-	}else if dLLTail==node{
-		dLLTail=node.Prev
-		node.Prev.Next=nil
-	}else{
-		node.Prev.Next=node.Next
-		node.next.prev=node.prev
+func (s dll[V]) delete(node ddlNode[V]) {
+	if s.dLLHead == node {
+		if s.dLLTail == node {
+			s.dLLTail = nil
+		}
+		s.dLLHead = s.dLLHead.next
+	} else if s.dLLTail == node {
+		s.dLLTail = s.dLLTail.prev
+		s.dLLTail.next = nil
+	} else {
+		node.prev.next = node.next
+		node.next.prev = node.prev
 	}
-		// delete(node)
+	node = nil // freeing memory will be tc by GC
 }
 
-func(s *dll)GetValue[V any](node *ddNode)(V,error){
-	if node!=nil{
-		return nil,fmt.Erorrs("empty node")
-	}
-	return node.value,nil
-
+func (s dll[V]) getValue(node ddlNode[V]) (V, error) {
+	return node.value, nil
 }
